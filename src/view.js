e17241a14622e33b1cbc1c4d4950443eef096ca5
Alisa Lobanova <17april94@gmail.com>
2022-08-30 23:20:03 +0300

    add feeds parsing

--- src/view.js
+++ src/view.js
@@ -1,12 +1,14 @@
 import * as yup from 'yup';
 import onChange from 'on-change';
 import i18next from 'i18next';
+import axios from 'axios';
 
 export default () => {
   const state = {
-    url: [],
+    urls: [],
     valid: true,
     error: '',
+    status: 'none',
   };
 
   i18next.init({
@@ -41,11 +43,15 @@ export default () => {
       pEl.classList.replace('text-success', 'text-danger');
       inputField.classList.add('is-invalid');
     }
+
+    if (state.status === 'parsing') {
+      parser(state.urls)
+    }
   });
 
   const schema = yup.string().required()
     .url('invalidUrl')
-    .notOneOf([watchedState.url], 'existingUrl');
+    .notOneOf([watchedState.urls], 'existingUrl');
 
   inputForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -54,11 +60,55 @@ export default () => {
     schema.validate(url).then(() => {
       watchedState.valid = true;
       watchedState.error = '';
-      watchedState.url.push(url);
+      watchedState.urls.push(url);
+      watchedState.status = 'parsing'
     })
       .catch((e) => {
         watchedState.valid = false;
         watchedState.error = e.message;
       });
   });
+
+  const parser = (urls) => {
+    const containerForFeeds = document.querySelector('.feeds');
+    containerForFeeds.innerHTML = '';
+    const card = document.createElement('div');
+    card.classList.add('card', 'border-0')
+    containerForFeeds.append(card);
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    card.append(cardBody);
+    const h2El = document.createElement('h2');
+    h2El.classList.add('card-title', 'h4')
+    h2El.textContent = 'Фиды'
+    cardBody.append(h2El);
+    const ulEl = document.createElement('ul');
+    ulEl.classList.add('list-group', 'border-0', 'rounded-0');
+    card.append(ulEl);
+    urls.forEach((url) => {
+      axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
+      .then((r) => {
+        const response = new DOMParser();
+        const ml = response.parseFromString(r.data.contents , 'text/html');
+        const title = ml.querySelector('title').textContent;
+        const description = ml.querySelector('description').textContent;
+        
+        const liEl = document.createElement('li');
+        liEl.classList.add('list-group-item', 'border-0', 'border-end-0');
+        ulEl.append(liEl); 
+        const hEl = document.createElement('h3');
+        hEl.classList.add('h6', 'm-0')
+        hEl.textContent = title;
+        const pEl = document.createElement('p');
+        pEl.classList.add('m-0', 'small', 'text-black-50')
+        pEl.textContent = description;
+        liEl.append(hEl);
+        liEl.append(pEl);
+        //console.log(title);
+        //console.log(ml)
+      })
+      .catch((e) => console.log(e))
+    })
+
+  }
 };
